a915a533bdb1f7dee2ea8827ccec43d042efe0b2
Gabor Bota <gabor.bota@instructure.com>
2025-01-04 12:20:25 +0100

    working

--- main.py
+++ main.py
@@ -13,17 +13,20 @@ import discord_bot
 with open('config.json', 'r') as f:
     config = json.load(f)
 CHECKPOINT_PATH = config['checkpoint_path']
+DEBUG_SAVE_OBSERVATION = False
+LIMIT_ACTION_SPACE = True
+RENDER_MODE_HUMAN = False
+TRAINING_EPISODES = 60000
 
 ## ENVIRONMENT
-LIMIT_ACTION_SPACE = True
-render = False
 gym.register_envs(ale_py)
-env = gym.make('ALE/SpaceInvaders-v5', render_mode="human" if render else None)
-env = wrappers.SkipFrame(env, skip=4)
+env = gym.make('ALE/SpaceInvaders-v5', render_mode="human" if RENDER_MODE_HUMAN else None)
+# env = wrappers.SkipFrame(env, skip=4)
 env = gym.wrappers.GrayscaleObservation(env, keep_dim=False)
 env = gym.wrappers.ResizeObservation(env, shape=(84, 84))
-env = gym.wrappers.TransformObservation(env, lambda obs: (obs / 255.0).astype(np.uint8), observation_space=env.observation_space)
 env = gym.wrappers.FrameStackObservation(env, 4)
+env = gym.wrappers.TransformObservation(env, lambda obs: obs / 255., observation_space=env.observation_space)
+
 if LIMIT_ACTION_SPACE:
     allowed_actions = [1,2,3,4,5]
     env = wrappers.ActionSpaceWrapper(env, allowed_actions)
@@ -33,6 +36,7 @@ print(f"Action space: {env.action_space}")
 observation, info = env.reset()
 
 ## CHECKPOINTS AND AGENT
+Path(CHECKPOINT_PATH).mkdir(parents=True, exist_ok=True)
 checkpoints = sorted(Path(CHECKPOINT_PATH).iterdir(), key=lambda x: x.name, reverse=True)
 # sort checkpoints by name
 # in this directory the checkpoinst are saved in .chkpt file extension. find the last checkpoint
@@ -49,20 +53,25 @@ print(f"Last checkpoint: {last_checkpoint}")
 save_dir.mkdir(parents=True)
 logger = metrics.MetricLogger(save_dir=save_dir)
 
-episodes = 60000
-episode_times = np.zeros(episodes)
-
+episode_times = np.zeros(TRAINING_EPISODES)
 agent = agent.Agent(
     state_dim=(4, 84, 84),
     action_dim=env.action_space.n,
     save_dir=save_dir,
-    iterations=episodes,
+    iterations=TRAINING_EPISODES,
     checkpoint=last_checkpoint
 )
 
+if DEBUG_SAVE_OBSERVATION:
+    # create the img directory if it doesn't exist
+    Path("img").mkdir(parents=True, exist_ok=True)
+    # delete all fles in the img directory
+    for file in Path("img").iterdir():
+        file.unlink()
+
 total_start_time = datetime.datetime.now()
 discord_bot.send(f"Training has started at {total_start_time}.")
-for e in range(episodes):
+for e in range(TRAINING_EPISODES):
     observation, info = env.reset()
 
     comm_reward = 0
@@ -70,7 +79,7 @@ for e in range(episodes):
     episode_start_time = datetime.datetime.now()
     while True:
         # 3. Show environment (the visual)
-        if render:
+        if RENDER_MODE_HUMAN:
             env.render()
 
         # 4. Run agent on the state
@@ -97,6 +106,17 @@ for e in range(episodes):
         if terminated or truncated:
             break
 
+        # save observation matrixes as text files to debug
+        if DEBUG_SAVE_OBSERVATION:
+            if e == 0:
+                # remove the contents of the img directory
+                env.unwrapped.ale.saveScreenPNG(f"img/{agent.curr_step}_ale_observation.png")
+                for i in range(4):
+                    # if any of the observation matrixes contain at least one non-zero value, save it as a text file
+                    if np.any(observation[i]):
+                        # save matrix as text file, with float64 precision
+                        np.savetxt(f"img/{agent.curr_step}_observation_{i}.txt", observation[i], fmt='%.16f')
+
     logger.log_episode()
 
     delta_time = datetime.datetime.now() - episode_start_time
@@ -105,7 +125,7 @@ for e in range(episodes):
     if e % 20 == 0:
         print(f"Episode {e} finished with reward {comm_reward} in {delta_time.total_seconds()} seconds.")
         average_episode_time = np.mean(episode_times[:e+1])
-        expected_time_remaining = str(datetime.timedelta(seconds=(episodes - e) * average_episode_time))
+        expected_time_remaining = str(datetime.timedelta(seconds=(TRAINING_EPISODES - e) * average_episode_time))
         print(f"The average episode time is {average_episode_time} seconds, the expected time remaining is {expected_time_remaining}.")
         logger.record(
             episode=e,
